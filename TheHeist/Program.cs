1ffd5e95c75a5ed7937035233af3af43331a6581
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-08-15 12:13:29 -0500

    Add project files.

---
+++ TheHeist/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheHeist
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Plan your heist");
+
+            var newMember = new TeamMember();
+        }
+    }
+}
---
+++ TheHeist/TeamMember.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace TheHeist
+{
+    class TeamMember
+    {
+        public string Name { get; set; } = "";
+        public int SkillLevel { get; set; } = 0;
+        public decimal CourageFactor { get; set; } = 0;
+
+        public TeamMember()
+        {
+            do
+            {
+                Console.WriteLine("Enter in the team member's name.");
+                var name = Console.ReadLine();
+                if (name == "")
+                {
+                    return;
+                }
+                else
+                {
+                    Name = name;
+                }
+            } while (Name == "");
+
+            Console.WriteLine("Enter in the team member's skill level.");
+            var skillLevel = Console.ReadLine();
+            SkillLevel = Convert.ToInt32(skillLevel);
+            Console.WriteLine("Enter in the team member's courage factor.(This should be between 0.0 and 2.0)");
+            var courageFactor = Console.ReadLine();
+            CourageFactor = Convert.ToDecimal(courageFactor);
+            DisplayInfo();
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Team Member Name: {Name}");
+            Console.WriteLine($"Skill Level: {SkillLevel}");
+            Console.WriteLine($"Courage Factor: {CourageFactor}");
+        }
+    }
+}
